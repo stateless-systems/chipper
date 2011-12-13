e1f7cd4850661ed8f81c7426aeeeeb7e0521df36
John Barratt <djon00@gmail.com>
2011-12-13 02:25:45 +0000

    Bundle in libstemmer with chipper

--- chipper.gemspec
+++ chipper.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name              = "chipper"
-  s.version           = "0.3.4"
-  s.date              = "2011-11-30"
+  s.version           = "0.3.5"
+  s.date              = "2011-12-13"
   s.authors           = "Bharanee Rathna"
   s.email             = "deepfryed@gmail.com"
   s.summary           = "twitter text extractor"
   s.description       = "twitter text extraction utilities"
   s.homepage          = "http://github.com/deepfryed/chipper"
-  s.files             = Dir["ext/**/*.{cc,h}"] + %w(README.rdoc ext/extconf.rb) + Dir["test/*.rb"]
+  s.files             = Dir["ext/**/*.{cc,h}"] + %w(README.rdoc ext/extconf.rb) + Dir["test/*.rb"] + Dir["libstemmer_c/*/*.{c,h,txt}"] + 
+    ["libstemmer_c/MANIFEST","libstemmer_c/Makefile","libstemmer_c/README","libstemmer_c/mkinc.mak","libstemmer_c/mkinc_utf8.mak"]   
   s.extra_rdoc_files  = %w(README.rdoc)
   s.extensions        = %w(ext/extconf.rb)
   s.require_paths     = %w(lib)
--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,6 +3,45 @@
 require 'mkmf'
 require 'fileutils'
 
+################################################################################
+################################################################################
+## Derived from ruby-stemmer https://github.com/aurelian/ruby-stemmer
+
+# FreeBSD make is gmake
+make= (RUBY_PLATFORM =~ /freebsd/)? 'gmake' : 'make'
+
+ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+LIBSTEMMER = File.join(ROOT, 'libstemmer_c')
+
+# MacOS architecture mess up
+if RUBY_PLATFORM =~ /darwin/
+  # see: #issue/3, #issue/5
+  begin
+    ENV['ARCHFLAGS']= "-arch " + %x[file #{File.expand_path(File.join(Config::CONFIG['bindir'], Config::CONFIG['RUBY_INSTALL_NAME']))}].strip!.match(/executable (.+)$/)[1] unless ENV['ARCHFLAGS'].nil?
+  rescue
+    $stderr << "Failed to get your ruby executable architecture.\n"
+    $stderr << "Please specify one using $ARCHFLAGS environment variable.\n"
+    exit
+  end
+  # see: #issue/9, #issue/6
+  # see: man compat
+  if ENV['COMMAND_MODE'] == 'legacy'
+    $stdout << "Setting compat mode to unix2003\n."
+    ENV['COMMAND_MODE']= 'unix2003'
+  end
+end
+
+# make libstemmer_c. unless we're cross-compiling.
+unless RUBY_PLATFORM =~ /i386-mingw32/
+  system "cd #{LIBSTEMMER}; #{make} libstemmer.o; cd #{ROOT};"
+  exit unless $? == 0
+end
+
+################################################################################
+################################################################################
+
+
+
 EXTCONFIG        = Object.const_get(defined?(RbConfig) ? :RbConfig : :Config)::CONFIG
 EXTCONFIG['CC']  = 'g++'
 EXTCONFIG['CPP'] = 'g++'
@@ -13,6 +52,9 @@ maj, min = version.split(/\./).values_at(0, 1).map(&:to_i)
 $CFLAGS  = '-pthread -Wno-sign-compare '
 $CFLAGS += maj > 3 && min > 5 ? '-Ofast ' : '-O3 '
 $CFLAGS += '-I/usr/include -I/opt/local/include -I/usr/local/include'
+
+$CFLAGS  += " -I#{File.expand_path(File.join(LIBSTEMMER, 'include'))} "
+$libs    += " -L#{LIBSTEMMER} #{File.expand_path(File.join(LIBSTEMMER, 'libstemmer.o'))} "
 
 def apt_install_hint pkg
   "sudo apt-get install #{pkg}"
@@ -36,7 +78,6 @@ end
 
 srcprefix = '{re2,src}'
 
-exit 1 unless library_installed? 'stemmer', apt_install_hint('libstemmer-dev')
 create_makefile 'chipper', srcprefix
 
 # NOTE mkmk.rb is shit, it does not allow for multiple source dirs - hence the following hack.
